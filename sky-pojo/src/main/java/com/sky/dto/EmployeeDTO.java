f927bbefb25bffd04084e31eb263fc85780d3d94
caokun <caokun@163.com>
2024-04-17 15:58:45 +0800

    take-out 新增员工功能

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,10 +1,12 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel("新增员工时的DTO对象")
 public class EmployeeDTO implements Serializable {
 
     private Long id;
